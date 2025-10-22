408512a14c9607c24dab28bcb69ad2335b66c4a4
SB <ijhollaw@uncg.edu>
2025-10-22 09:00:48 -0400

    added spring initializr
    added application properties

---
+++ Yarah/src/main/java/com/backend_API/Yarah/YarahApplication.java
@@ -0,0 +1,13 @@
+package com.backend_API.Yarah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YarahApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YarahApplication.class, args);
+	}
+
+}
---
+++ Yarah/src/test/java/com/backend_API/Yarah/YarahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_API.Yarah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YarahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
