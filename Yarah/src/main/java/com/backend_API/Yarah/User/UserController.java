5d7599dc2a240de829027f56ce3aa8ca5cf56797
yera888 <yeraldinet1234@gmail.com>
2025-10-28 12:32:56 -0400

    Changes step 1

--- Yarah/src/main/java/com/backend_API/Yarah/User/UserController.java
+++ Yarah/src/main/java/com/backend_API/Yarah/User/UserController.java
@@ -1,23 +1,19 @@
-package com.backend_API.Yarah.User;
-
-import com.csc340340.backend_API.Yarah.customer.CustomerService;
+package com.backend_API.Yarah.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-import java.util.List;
-
 public class UserController {
     private final UserService userService;
-    private final CustomerServie customerService;
 
-    @PostMapping 
-    public ResponseEntity<User> createUser(@Valid)
+    @PutMapping("/{sellerId}")
+    public ResponseEntity<User> updateUser (@PathVariable("sellerId") Long userId, @Valid @RequestBody User userDetails) {
+        return ResponseEntity.ok(userService.updateUser(userId, userDetails));
+    }
 
 }
