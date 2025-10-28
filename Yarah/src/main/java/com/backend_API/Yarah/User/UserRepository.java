e166a4d49d1dd728134167d7a302ba9ecb3d4a54
SB <ijhollaw@uncg.edu>
2025-10-28 11:11:50 -0400

    updated backend
    fixed merge errors

--- Yarah/src/main/java/com/backend_API/Yarah/User/UserRepository.java
+++ Yarah/src/main/java/com/backend_API/Yarah/User/UserRepository.java
@@ -1,5 +1,12 @@
-package com.backend_API.Yarah.User;
+package com.backend_API.Yarah.user;
 
-public class UserRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
