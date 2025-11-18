99b2bcee03eedda9e6937ea243632b43ad369d76
SB <ijhollaw@uncg.edu>
2025-11-18 09:20:41 -0500

    updated backend
    removed unused imports

--- Yarah/src/main/java/com/backend_API/Yarah/user/UserController.java
+++ Yarah/src/main/java/com/backend_API/Yarah/user/UserController.java
@@ -5,8 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
