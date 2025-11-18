b9bc0e8db862e52d39c245c1f2d12e95b0b6b5dc
SB <ijhollaw@uncg.edu>
2025-11-18 06:15:17 -0500

    Update User.java
    removed unused imports

--- Yarah/src/main/java/com/backend_API/Yarah/user/User.java
+++ Yarah/src/main/java/com/backend_API/Yarah/user/User.java
@@ -7,10 +7,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
